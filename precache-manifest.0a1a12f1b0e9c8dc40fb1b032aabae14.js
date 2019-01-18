b3b98c480cc01aaf39b6520bd5894181464b53ef
Naeem Hasan <naeemhasan252@gmail.com>
2019-01-18 21:48:01 +0600

    Initial

---
+++ precache-manifest.0a1a12f1b0e9c8dc40fb1b032aabae14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fb85952bc43949e2662a",
+    "url": "/static/js/main.fb85952b.chunk.js"
+  },
+  {
+    "revision": "30dd7868c0edc3c93fa2",
+    "url": "/static/js/1.30dd7868.chunk.js"
+  },
+  {
+    "revision": "fb85952bc43949e2662a",
+    "url": "/static/css/main.71460be0.chunk.css"
+  },
+  {
+    "revision": "73ff97607dc93b1bda096363e6a93e40",
+    "url": "/index.html"
+  }
+];
